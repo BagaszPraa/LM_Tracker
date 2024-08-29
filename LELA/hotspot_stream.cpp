d58d93f59d667a74bf18a57b75c4bc014bc127b3
BagaszPraa <bagaszpraa@gmail.com>
2024-08-29 10:38:08 +0700

    LELA

--- LELA/hotspot_stream.cpp
+++ LELA/hotspot_stream.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
     string ip_address = argv[3];          // Alamat IP target dari argumen ketiga
     int port = stoi(argv[4]);             // Port target dari argumen keempat
 
-    VideoCapture cap(0);
+    VideoCapture cap("/dev/video0");
     if (!cap.isOpened()) {
         cout << "Gagal membuka kamera." << endl;
         return -1;
